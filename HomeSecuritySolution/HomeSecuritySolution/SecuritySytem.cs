f3d92565ce556a1a5bab5776c1d9825d6380eff6
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-17 22:13:49 +0530

    Update SecuritySytem.cs

--- HomeSecuritySolution/HomeSecuritySolution/SecuritySytem.cs
+++ HomeSecuritySolution/HomeSecuritySolution/SecuritySytem.cs
@@ -6,13 +6,8 @@ using System.Threading.Tasks;
 
 namespace HomeSecuritySolution
 {
-    class SecuritySytem
-    {
-            var time = System.DateTime.Now;
-            String date = time.ToString("yyyy,MM,dd,hh");
-
-            string message = $"Door {status} and time is {time} {date}";
-            Console.WriteLine(message);
- 
-    }
+     public void AlertMessage(string status)
+        {
+            Console.WriteLine(status);
+        }
 }
